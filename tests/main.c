bf75af05d0f5c41363a56db1cd79525ab80314b6
Adam Smith <iihtimsad@gmail.com>
2014-10-04 20:08:47 -0400

    More tests...

--- tests/main.c
+++ tests/main.c
@@ -4,5 +4,12 @@
 int main(int argc, char *argv[]) {
 	// TODO: add more tests...
 	printfc("[r]%s %s\n", "red!", "normal!");
+	printfc(" - [r]%s\n", "red");
+	printfc(" - [g]%s\n", "green");
+	printfc(" - [y]%s\n", "yellow");
+	printfc(" - [b]%s\n", "blue");
+	printfc(" - [m]%s\n", "magenta");
+	printfc(" - [c]%s\n", "cyan");
+	printfc(" - [n]%s\n", "normal");
     return 0;
 }
