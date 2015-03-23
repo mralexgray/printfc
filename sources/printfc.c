b9ea8222f4273878c9ddfc90a4daa386c80900f0
Alex Gray <alex@mrgray.com>
2015-03-23 11:35:56 -0400

    stylistic differences

--- sources/printfc.c
+++ sources/printfc.c
@@ -4,95 +4,59 @@
 #include <ctype.h>
 #include "printfc.h"
 
-
 // ANSI escape codes.
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define MAGENTA "\x1b[35m"
-#define CYAN    "\x1b[36m"
-#define NORMAL   "\x1b[0m"
-
+#define RED            "\x1b[31m"
+#define GREEN           "\x1b[32m"
+#define YELLOW          "\x1b[33m"
+#define BLUE            "\x1b[34m"
+#define MAGENTA         "\x1b[35m"
+#define CYAN            "\x1b[36m"
+#define NORMAL          "\x1b[0m"
+#define OTHER(X)        "\x1b[38;5;"#X"m"
 
 // Maps format characters to color codes.
-static const char *ccmap(char c)
-{
-	switch(c){
-	case 'r': return RED;
-	case 'g': return GREEN;
-	case 'y': return YELLOW;
-	case 'b': return BLUE;
-	case 'm': return MAGENTA;
-	case 'c': return CYAN;
-	case 'n': return NORMAL;
-	default:
-		printfc("[r]%s[-]%c\n", " - ERROR: invalid color specifier: ", c);
-		exit(1);
-	}
-}
 
+static const char *ccmap(char c) { return
+
+    c == 'r' ? RED     : c == 'g' ? GREEN : c == 'y' ? YELLOW : c == 'b' ? BLUE :
+    c == 'm' ? MAGENTA : c == 'c' ?  CYAN : c == 'n' ? NORMAL : OTHER(c);
+
+// ({ printfc("[r]%s[-]%c\n", " - ERROR: invalid color specifier: ", c); exit(1); NORMAL; });
+}
 
 // Counts occurences of character in string.
-static int howMany(const char *s, char c)
-{
-	int n = 0;
-	while(*s){
-		if(*s == c)
-			++n;
-		++s;
-	}
-	return n;
-}
 
+__unused static int howMany(const char *s, char c) { int n = 0; while(*s){ if(*s == c) ++n; ++s; } return n; }
 
 // Appends character to string.
-static void cappend(char *s, char c)
-{
-	s += strlen(s);
-	s[0] = c;
-	s[1] = 0;
-}
 
+static void cappend(char *s, char c) { s += strlen(s); s[0] = c; s[1] = 0; }
 
-// Reads printf format string, replacing color-indicating
-// characters with their respective color codes.
-static char *ccformat(const char *f)
-{
-	char *r = malloc(strlen(f) * 8);
-	if(!r)
-		return NULL;
-	*r = 0;
+// Reads printf fmt str, replacing color-indicating chars with respective color codes.
+
+static char *ccformat(const char *f) {
+
+	char *r; if(!(r = malloc(strlen(f) * 8))) return NULL; *r = 0;
 
 	strcat(r, NORMAL);
+
 	while(*f){
+
 		if(f[0] == '[' && f[2] == ']'  && f[3] == '%'){
+
 			strcat(r, ccmap(f[1])), f += 3;
-			while(!isalpha(*f))
-				cappend(r, *f), ++f;
+			while(!isalpha(*f)) cappend(r, *f), ++f;
 			cappend(r, *f), ++f;
 			strcat(r, NORMAL);
-		} else{
-			cappend(r, *f);
-			++f;
-		}
+
+    } else{ cappend(r, *f); ++f; }
 	}
 	strcat(r, NORMAL);
 	return r;
 }
 
-
 // Main printing function.
-int printfc(const char *cf, ...)
-{
-	va_list args;
-	va_start(args, cf);
-	char *f = ccformat(cf);
-	if(f){
-		vprintf(f, args);
-		free(f);
-		va_end(args);
-		return 0;
-	}
-	return 1;
+int printfc(const char *cf, ...) { va_list args; va_start(args, cf); char *f;
+
+  if(!(f = ccformat(cf))) return 1; vprintf(f, args); free(f); return va_end(args), 0;
 }
--- tests/main.c
+++ tests/main.c
@@ -2,8 +2,12 @@
 
 
 int main(int argc, char *argv[]) {
+
 	// TODO: add more tests...
 	printfc("[r]%s %s\n", "red!", "normal!");
+
+  printfc("[r]%s %s [r]%s\n", "red!", "and", "RED!");
+
 	printfc(" - [r]%s\n", "red");
 	printfc(" - [g]%s\n", "green");
 	printfc(" - [y]%s\n", "yellow");
@@ -11,5 +15,16 @@ int main(int argc, char *argv[]) {
 	printfc(" - [m]%s\n", "magenta");
 	printfc(" - [c]%s\n", "cyan");
 	printfc(" - [n]%s\n", "normal");
+  
+  printfc(" - [122]%s\n", "nonsense");
+
+  printfc(" - [r]%s\n"
+          " - [g]%s\n"
+          " - [y]%s\n"
+          " - [b]%s\n"
+          " - [m]%s\n"
+          " - [c]%s\n"
+          " - [n]%s\n", "yellow","red", "green","blue", "magenta", "cyan", "normal");
+
     return 0;
 }
